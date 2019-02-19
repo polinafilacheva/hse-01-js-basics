ab404278339c88da9c679773b730a8b668fe9bee
polinafilacheva <polina.filacheva@gmail.com>
2019-02-19 10:52:38 +0300

    практика2

--- 03/practise.js
+++ 03/practise.js
@@ -23,22 +23,41 @@
 //создаем функцию, которая принимает значение месяцев весна лето зима осень
 //
 
-function checkSeason(month) {
-   if(month >= 3 && month <= 5) {
-     console.log("Весна");
-   }
+// function checkSeason(month) {
+//    if(month >= 3 && month <= 5) {
+//      console.log("Весна");
+//    }
+//
+//    if (month >= 6 && month <=8) {
+//      console.log("Лето");
+//    }
+//
+//    if (month >=9 && month <= 11) {
+//     console.log("Осень");
+//    }
+//
+//    if (month === 12 || month === 1 || month === 2) {
+//     console.log("Зима");
+//   }
+// }
+// let askSeason = prompt("КАкой месяц")
+// checkSeason(Number(askSeason))
 
-   if (month >= 6 && month <=8) {
-     console.log("Лето");
-   }
 
-   if (month >=9 && month <= 11) {
-    console.log("Осень");
-   }
 
-   if (month === 12 || month === 1 || month === 2) {
-    console.log("Зима");
-  }
-}
-let askSeason = prompt("КАкой месяц")
-checkSeason(Number(askSeason))
+
+// function checkBedTime(wakeUpTime) {
+// 	  let sleepingTime = 8
+// 	  if (wakeUpTime < 0 || wakeUpTime > 23) {
+// 	    console.error("Время должно быть от 0 до 23")
+// 	  } else {
+// 	    let bedTime = wakeUpTime - sleepingTime
+// 	    if (bedTime < 0) {
+// 	      console.log("Ложитесь спать в: " + (24 + bedTime))
+// 	    } else {
+// 	      console.log("Ложитесь спать в: " + bedTime)
+// 	    }
+// 	  }
+// 	}
+// 	checkBedTime(9)
+// 	checkBedTime(7)
