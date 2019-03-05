c12f75b9c2e7d9a53231f631f8ad6ae066391ab0
polinafilacheva <polina.filacheva@gmail.com>
2019-03-05 11:03:09 +0300

    доделали код с лошадьми

--- 04/traking.js
+++ 04/traking.js
@@ -1,5 +1,30 @@
-let scores = [22, 33, 42, 54, 65,76, 44, 24, 65]
-let i = 0
-while (i < scores.length) {
-  let msg = `Лошадь №${i + 1} пришла с результатом ${scores[i]}`
+let scores = [22, 33, 42, 54, 65,76, 44, 24, 65, 90]
+// let i = 0
+//
+// while (i < scores.length) {
+//   if (scores[i] > highScore) {
+//     highScore = scores[i]
+//   }
+//   let msg = `Лошадь №${i + 1} пришла с результатом ${scores[i]}`
+//   console.log(msg)
+//   i += 1
+// }
+
+
+let highScore = 0
+for (let i = 0; i < scores.length; i += 1) {
+  if (scores[i] > highScore) {
+    highScore = scores[i]
 }
+let msg = `Лошадь №${i + 1} пришла с результатом ${scores[i]}`
+console.log(msg)
+}
+console.log(highScore);
+
+let bestScores = []
+for (let i = 0; i < scores.length; i += 1) {
+  if (scores[i] === highScore) {
+    bestScores.push(scores[i])
+  }
+}
+console.log(bestScores);
